0c897ac653c9882da1101b78ef7f3f69a719cc7c
Thomaz Alonso de Miranda Kettelut Polon <tadmkpol@marfrig.com.br>
2021-10-27 16:07:02 -0300

    Iniciado tratamento para consulta no google

--- SisGoo/SisGoo/Services/Implementations/ConsultaServiceImplementation.cs
+++ SisGoo/SisGoo/Services/Implementations/ConsultaServiceImplementation.cs
@@ -1,7 +1,9 @@
 ﻿using SisGoo.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SisGoo.Services.Implementations
@@ -11,6 +13,21 @@ namespace SisGoo.Services.Implementations
         public List<Consulta> Buscar(string frase)
         {
             List<Consulta> consultas = new List<Consulta>();
+
+            var client = new HttpClient();
+
+            // Get the response.
+            //HttpResponseMessage response = await client.GetAsync("https://www.google.com/search?q=teste&rlz=1C1GCEB_enBR959BR959&oq=teste&aqs=chrome..69i57j69i59j0i433i512j0i512l2j0i433i512l5.4811j0j15&sourceid=chrome&ie=UTF-8");
+
+            // Get the response content.
+            //HttpContent responseContent = response.Content;
+
+            // Get the stream of the content.
+            //using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+            //{
+                // Write the output.
+                //var retorno = await reader.ReadToEndAsync();
+            //}
 
             Consulta consultaTeste = new Consulta
             {
